f1e848b05521153ccfc313d3c5d92b81a4ae0977
Safia <mohammad.safiah@mawdoo3.com>
2022-02-27 15:26:57 +0300

    create e2e test for auth

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
-const cookieSession = require('cookie-session');
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['safia']
-  }));
-  
   const config = new DocumentBuilder()
     .setTitle('Cars reports')
     .setDescription('The car API description')
@@ -21,9 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
   await app.listen(3000);
 }
 
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -3,19 +3,28 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Authentication System (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    expect(app).toBeDefined()
+  it('handles a signup request', () => {
+    const email = 'afjs@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
   });
 });
